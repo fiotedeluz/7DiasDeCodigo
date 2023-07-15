2ed081b88c01b60fc6688c65b0e9b3b691405446
Gleidson <v3vgleid@gmail.com>
2023-07-15 00:33:57 -0300

    projeto incompleto dia3

---
+++ 7dias de codigo/thirdDay/terceiroDia.js
@@ -0,0 +1,21 @@
+
+// todos os nomes de variaveis são comicamente aleatorios por motivo nenhum
+alert("ah, ola desenvlvedor. deixo aqui algumas questões rapidas")
+var jorge = prompt ("voce quer seguir area front-end ou back-end?")
+alert("entendo")
+if (jorge == 'front-end' ) {
+    ruan = prompt (" mas voce iria preferir aprender React ou Vue?")
+}   else if (jorge == 'front end' ) {
+    ruan = prompt (" mas voce iria preferir aprender React ou Vue?")
+}
+
+if (jorge == 'back-end') {
+    ruan = prompt (" e então voce iria preferia aprender C# ou aprender Java?")
+} else if ( jorge == 'back end') {
+    ruan = prompt (" e então voce iria preferia aprender C# ou aprender Java?")
+} 
+
+
+
+//www.google.com.br/search?q=qualquer+coisa
+//https://pt.wikipedia.org/w/index.php?search=
