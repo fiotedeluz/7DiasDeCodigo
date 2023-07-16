07136d0a379af65b8714aeb2bb52f1d7f692df37
Gleidson <v3vgleid@gmail.com>
2023-07-15 22:46:32 -0300

    finalizando o terceiroDia

--- 7dias de codigo/thirdDay/terceiroDia.js
+++ 7dias de codigo/thirdDay/terceiroDia.js
@@ -2,20 +2,27 @@
 // todos os nomes de variaveis são comicamente aleatorios por motivo nenhum
 alert("ah, ola desenvlvedor. deixo aqui algumas questões rapidas")
 var jorge = prompt ("voce quer seguir area front-end ou back-end?")
-alert("entendo")
+alert("hmm")
 if (jorge == 'front-end' ) {
     ruan = prompt (" mas voce iria preferir aprender React ou Vue?")
 }   else if (jorge == 'front end' ) {
     ruan = prompt (" mas voce iria preferir aprender React ou Vue?")
-}
-
-if (jorge == 'back-end') {
+} else if (jorge == 'back-end') {
     ruan = prompt (" e então voce iria preferia aprender C# ou aprender Java?")
 } else if ( jorge == 'back end') {
     ruan = prompt (" e então voce iria preferia aprender C# ou aprender Java?")
-} 
+} else alert("Você não inseriu um valor válido!")
 
+alert("ok");
+if (ruan == "react") {
+    alert (`essa e uma tecnologia muito legal voce poderia procurar pelo ${ruan} na alura`)
+} else if (ruan == "vue") {
+    alert (`essa e uma tecnologia muito legal voce poderia procurar pelo ${ruan} na alura`)
+} else if (ruan == "C#") {
+    alert (`essa e uma tecnologia muito legal voce poderia procurar pelo ${ruan} na alura`)
+} else if (ruan == "java") {
+    alert (`essa e uma tecnologia muito legal voce poderia procurar pelo ${ruan} na alura`)
+} else {
+    alert(`hmm, talvez ${ruan} seja melhor mesmo`)
+}
 
-
-//www.google.com.br/search?q=qualquer+coisa
-//https://pt.wikipedia.org/w/index.php?search=
