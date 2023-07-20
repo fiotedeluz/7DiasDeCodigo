444b64cb9f31def01c213fc5e9ab1e5457746a45
Gleidson <v3vgleid@gmail.com>
2023-07-20 00:30:23 -0300

    quinto dia incompleto

--- 7dias de codigo/5fifthDay/quintoDia.js
+++ 7dias de codigo/5fifthDay/quintoDia.js
@@ -5,24 +5,37 @@ var doce = []
 var verdura = []
 
 alert ("bem vindo a lista de compras")
-var permissao = prompt=("voce deseja adicionar algo a sua lista")
-if (permissao = "sim") {
-    var clasce = prompt("seu produto é uma fruta, verdura, laticinio, doce ou congelado?")
+let repetiçao = true;
+while (repetiçao) {
+    var permissao = prompt("voce deseja adicionar algo a sua lista")
+    if (permissao = "sim") {
+        var clasce = prompt("seu produto é uma fruta, verdura, laticinio, doce ou congelado?")
 
-     if (clasce = fruta) {
-        var produto = prompt("e qual o produto?")
-        fruta.push (`${produto}`);
-     } else     if (clasce = verdura) {
-        var produto = prompt("e qual o produto?")
-        verdura.push (`${produto}`);
-     } else     if (clasce = laticinio) {
-        var produto = prompt("e qual o produto?")
-        laticinio.push (`${produto}`);
-     } else     if (clasce = doce) {
-        var produto = prompt("e qual o produto?")
-        doce.push (`${produto}`);
-     } else     if (clasce = congelado) {
-        var produto = prompt("e qual o produto?")
-        congelado.push (`${produto}`);
-     }
+        if (clasce = fruta) {
+            var produto = prompt("e qual o produto?")
+            fruta.push (`${produto}`);
+        } else     if (clasce = verdura) {
+            var produto = prompt("e qual o produto?")
+            verdura.push (`${produto}`);
+        } else     if (clasce = laticinio) {
+            var produto = prompt("e qual o produto?")
+            laticinio.push (`${produto}`);
+        } else     if (clasce = doce) {
+            var produto = prompt("e qual o produto?")
+            doce.push (`${produto}`);
+        } else     if (clasce = congelado) {
+            var produto = prompt("e qual o produto?")
+            congelado.push (`${produto}`);
+        }
+    }
+    restart = prompt("digite ok para continuar adicionando")
+    if (restart !== "ok") {
+        repetiçao = false;
+    }
 }
+
+    console.log (fruta)
+    console.log (verdura)
+    console.log (laticinio)
+    console.log (doce)
+    console.log (congelado)
