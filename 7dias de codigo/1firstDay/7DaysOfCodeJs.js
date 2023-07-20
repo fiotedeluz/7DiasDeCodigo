53100741598da006301c25a02ef3681fb93a842b
Gleidson <v3vgleid@gmail.com>
2023-07-19 23:17:47 -0300

    recolocando tudo

---
+++ 7dias de codigo/1firstDay/7DaysOfCodeJs.js
@@ -0,0 +1,33 @@
+console.log ('dia UM')
+
+var UmNumero = 1;
+var UmString = '1';
+var DezNumero = 10;
+var DezString = '10';
+
+console.log ("comparando umNumero com umString")
+console.log ('(UmNumero == UmString)')
+if (UmNumero == UmString) {
+    console.log ("verdadeiro")
+} else {
+    console.log ("falso")
+}
+
+console.log ("confesso nao ter compreendido o objetivo mas em caso de tentarmos o mesmo projeto para que ele identifique que o umNumero e o umString são tipos diferentes de informação")
+
+console.log ("podemos tentar")
+
+console.log ("comparando de forma especifica umNumero com umString")
+console.log ('(UmNumero === UmString)')
+if (UmNumero === UmString) {
+    console.log ("verdadeiro")
+} else {
+    console.log ("falso")
+}
+
+console.log ("para concluir voce pode tambem tentar")
+var x = 1
+var y = '1'
+console.log (" x é igual a y, mas foram escritos em tipos diferentes. logo")
+console.log ("x == y = (true) mas x === y = (false)")
+    console.log ("n some os dois... 1+1 nao e dois....")
