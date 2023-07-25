ff120dca52d54c7164ae60e128ce51e6d5982ac1
Gleidson <v3vgleid@gmail.com>
2023-07-24 22:06:10 -0300

    terminado o quintoDia

--- 7dias de codigo/5fifthDay/quintoDia.js
+++ 7dias de codigo/5fifthDay/quintoDia.js
@@ -5,37 +5,36 @@ var doce = []
 var verdura = []
 
 alert ("bem vindo a lista de compras")
-let repetiçao = true;
+var repetiçao = true;
 while (repetiçao) {
-    var permissao = prompt("voce deseja adicionar algo a sua lista")
-    if (permissao = "sim") {
-        var clasce = prompt("seu produto é uma fruta, verdura, laticinio, doce ou congelado?")
+    var produto = prompt("oque deseja adicionar a lista")
+    var clasce = prompt("seu produto é uma fruta, verdura, laticinio, doce ou congelado?")
 
-        if (clasce = fruta) {
-            var produto = prompt("e qual o produto?")
-            fruta.push (`${produto}`);
-        } else     if (clasce = verdura) {
-            var produto = prompt("e qual o produto?")
-            verdura.push (`${produto}`);
-        } else     if (clasce = laticinio) {
-            var produto = prompt("e qual o produto?")
-            laticinio.push (`${produto}`);
-        } else     if (clasce = doce) {
-            var produto = prompt("e qual o produto?")
-            doce.push (`${produto}`);
-        } else     if (clasce = congelado) {
-            var produto = prompt("e qual o produto?")
-            congelado.push (`${produto}`);
-        }
+    if (clasce === `fruta`) {
+       fruta.push (`${produto}`);
+    } else     if (clasce === `verdura`) {
+       verdura.push (`${produto}`);
+    } else     if (clasce === `laticinio`) {
+       laticinio.push (`${produto}`);
+    } else     if (clasce === `doce`) {
+       doce.push (`${produto}`);
+    } else     if (clasce === `congelado`) {
+       congelado.push (`${produto}`);
+    } else if ( clasce != `fruta` && `verdura` && `laticinio` && `doce` && `congelado`) {
+        alert("essa categoria não foi predefinida")
     }
+
+        
     restart = prompt("digite ok para continuar adicionando")
     if (restart !== "ok") {
         repetiçao = false;
     }
-}
 
-    console.log (fruta)
-    console.log (verdura)
-    console.log (laticinio)
-    console.log (doce)
-    console.log (congelado)
+
+}
+console.log (fruta)
+console.log (verdura)
+console.log (laticinio)
+console.log (doce)
+console.log (congelado)
+    
